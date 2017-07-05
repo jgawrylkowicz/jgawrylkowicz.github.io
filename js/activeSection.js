7e2dede1558b2c50095fae52fae37a5d013991df
Jakub Gawrylkowicz <jgawrylkowicz@gmail.com>
2017-07-05 14:48:04 +0200

    update mobile support and add javascript for active section

---
+++ js/activeSection.js
@@ -0,0 +1,50 @@
+/**
+ * Created by jakub on 05.07.17.
+ */
+function showActiveSection(){
+
+    var activeSection = null;
+    var fadeSpeed = 300;
+    var verticalScrollHeight = parseInt( $(window).scrollTop());
+
+
+    if ($(window).width() <= 575) {
+        var introSectionHeight = $('#intro').height();
+        var projectsSectionHeight = $('#projects').height() + introSectionHeight;
+        var contactSectionHeight = $('#contact').height() + projectsSectionHeight;
+
+        if (verticalScrollHeight <= introSectionHeight) {
+            activeSection = 'Home';
+            $('.active-section').html(activeSection);
+        }
+        else if (verticalScrollHeight > introSectionHeight
+            && verticalScrollHeight <= projectsSectionHeight) {
+            activeSection = 'Projects';
+            $('.active-section').html(activeSection);
+        }
+        else if (verticalScrollHeight > projectsSectionHeight
+            && verticalScrollHeight <= contactSectionHeight) {
+            activeSection = 'Contact';
+            $('.active-section').html(activeSection);
+        }
+
+
+        //TODO fade in and fade out
+
+        /*function changeTo(value) {
+
+         if (value === activeSection || activeSection === null) {
+
+         $('.active-section').fadeOut(fadeSpeed, function () {
+         $('.active-section').html(value);
+         $('.active-section').fadeIn(fadeSpeed);
+         });
+         }
+         }
+
+         */
+
+    }
+}
+
+window.onload = showActiveSection();
