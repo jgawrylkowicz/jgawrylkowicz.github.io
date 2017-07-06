d5f5fc305a61dd794c4abaf9c772443c591b2c03
Jakub Gawrylkowicz <jgawrylkowicz@gmail.com>
2017-07-06 14:50:04 +0200

    update android support

--- js/activeSection.js
+++ js/activeSection.js
@@ -7,44 +7,41 @@ function showActiveSection(){
     var fadeSpeed = 300;
     var verticalScrollHeight = parseInt( $(window).scrollTop());
 
+    var introSectionHeight = $('#intro').height();
+    var projectsSectionHeight = $('#projects').height() + introSectionHeight;
+    var contactSectionHeight = $('#contact').height() + projectsSectionHeight;
 
-    if ($(window).width() <= 575) {
-        var introSectionHeight = $('#intro').height();
-        var projectsSectionHeight = $('#projects').height() + introSectionHeight;
-        var contactSectionHeight = $('#contact').height() + projectsSectionHeight;
-
-        if (verticalScrollHeight <= introSectionHeight) {
-            activeSection = 'Home';
-            $('.active-section').html(activeSection);
-        }
-        else if (verticalScrollHeight > introSectionHeight
-            && verticalScrollHeight <= projectsSectionHeight) {
-            activeSection = 'Projects';
-            $('.active-section').html(activeSection);
-        }
-        else if (verticalScrollHeight > projectsSectionHeight
-            && verticalScrollHeight <= contactSectionHeight) {
-            activeSection = 'Contact';
-            $('.active-section').html(activeSection);
-        }
-
-
-        //TODO fade in and fade out
-
-        /*function changeTo(value) {
-
-         if (value === activeSection || activeSection === null) {
-
-         $('.active-section').fadeOut(fadeSpeed, function () {
-         $('.active-section').html(value);
-         $('.active-section').fadeIn(fadeSpeed);
-         });
-         }
-         }
-
-         */
-
+    if (verticalScrollHeight <= introSectionHeight) {
+        activeSection = 'Home';
+        $('.active-section').html(activeSection);
     }
+    else if (verticalScrollHeight > introSectionHeight
+        && verticalScrollHeight <= projectsSectionHeight) {
+        activeSection = 'Projects';
+        $('.active-section').html(activeSection);
+    }
+    else if (verticalScrollHeight > projectsSectionHeight
+        && verticalScrollHeight <= contactSectionHeight) {
+        activeSection = 'Contact';
+        $('.active-section').html(activeSection);
+    }
+
+
+    //TODO fade in and fade out
+
+    /*function changeTo(value) {
+
+     if (value === activeSection || activeSection === null) {
+
+     $('.active-section').fadeOut(fadeSpeed, function () {
+     $('.active-section').html(value);
+     $('.active-section').fadeIn(fadeSpeed);
+     });
+     }
+     }
+
+     */
+
 }
 
 window.onload = showActiveSection();
