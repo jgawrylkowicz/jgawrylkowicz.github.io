518171a7d60fe21c4bb56f92e72a718d1602859b
Jakub Gawrylkowicz <jgawrylkowicz@gmail.com>
2017-07-08 14:15:57 +0200

    first try by targeting clients via JS

--- js/userAgent.js
+++ js/userAgent.js
@@ -10,16 +10,31 @@ function resizeSection() {
 
     if (navigator.userAgent.match(/(iPad|iPhone|iPod)/i)) {
         userAgent = 'device-ios';
+        $('#navbar')
+            .removeClass('navbar-toggleable-md')
+            .addClass('navbar-toggleable-lg');
+
     }
     else if (navigator.userAgent.match(/android/i)){
         userAgent = 'device-android';
+
+        //chrome
+        $('#navbar')
+            .removeClass('navbar-toggleable-md')
+            .addClass('navbar-toggleable-lg');
+
+        $('#page-top').css('font-size', '2rem');
+
     }
     else{
 
-        $('#navbar').removeClass('navbar-toggleable-md').addClass('navbar-toggleable');
+        userAgent = 'desktop';
+        $('#navbar')
+            .removeClass('navbar-toggleable-md')
+            .addClass('navbar-toggleable');
         $('#navbar-brand').addClass('hidden-sm-up');
 
-        userAgent = 'desktop';
+
     }
 
     $('.results').html('dpi: ' + dpi + ' window:' + $(window).width() + 'uA: ' +userAgent);
